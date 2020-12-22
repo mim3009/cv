c30fc102c73e0ebd20fc3d618966ebe137713c9c
Roman Zhyhulskyi <roman.zhyhulskyi@valtech.com>
2020-12-22 21:55:16 +0200

    added autoprefixer

--- app/webpack.config.js
+++ app/webpack.config.js
@@ -41,7 +41,7 @@ module.exports = {
 			},
 			{
 				test: /\.s[ac]ss$/i,
-				use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+				use: [MiniCssExtractPlugin.loader, 'css-loader', 'postcss-loader', 'sass-loader'],
 			},
 			{
 				test: /\.(woff|woff2|ttf|otf)$/,
